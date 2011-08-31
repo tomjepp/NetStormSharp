b32497d10d4137999156adac64402638b0b6c031
Thomas Jepp <tom@tomjepp.co.uk>
2011-09-01 00:28:45 +0100

    Add TestApp for testing NetStormSharp features

---
+++ TestApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using NetStormSharp.TitanArc;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fs = new FileStream(@"D:\Gaming\NetStorm\NetStorm\tarcExtract\netstorm.tarc", FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                TarcFile tarc = new TarcFile(fs);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
