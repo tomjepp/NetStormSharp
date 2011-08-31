08d5b520e60f51acc8722e193a64dee4af11494e
Thomas Jepp <tom@tomjepp.co.uk>
2011-08-31 23:12:45 +0100

    Initial code commit - start implementing TitanArc files

---
+++ NetStormSharp/Utility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetStormSharp
+{
+    public static class Utility
+    {
+        public static bool CompareBytes(byte[] array1, byte[] array2)
+        {
+            if (array1 == null || array2 == null)
+                return false;
+
+            if (array1.Length != array2.Length)
+                return false;
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] != array2[i])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
