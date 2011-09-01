0f25412a2ef9cb750e54a3c66a2fec522b2b3599
Thomas Jepp <tom@tomjepp.co.uk>
2011-09-01 01:02:15 +0100

    Finish palette loading implementation

--- NetStormSharp/Shapes/Palette.cs
+++ NetStormSharp/Shapes/Palette.cs
@@ -6,9 +6,35 @@ namespace NetStormSharp.Shapes
 {
     public class Palette
     {
+        private PaletteColor[] m_Entries;
+
+        public PaletteColor[] Entries
+        {
+            get
+            {
+                return m_Entries;
+            }
+        }
+
         public Palette(Stream stream)
         {
-            
+            PaletteHeader header = stream.ReadStruct<PaletteHeader>();
+
+            if (header.ID != 0xB123)
+                throw new Exception("Unable to load palette. ID != 0xB123.");
+
+            if (header.Version != 0)
+                throw new Exception("Unable to load palette. Version != 0.");
+
+            uint entryCount = header.FileLength / 3;
+
+            m_Entries = new PaletteColor[entryCount];
+            for (uint i = 0; i < entryCount; i++)
+            {
+                PaletteColor color = stream.ReadStruct<PaletteColor>();
+                m_Entries[i] = color;
+            }
+
             stream.Dispose();
         }
     }
